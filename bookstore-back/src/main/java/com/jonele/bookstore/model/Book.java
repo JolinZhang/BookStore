2e0ad342e9b855607bc131a3285602ea527475fd
Jolin Zhang <zhangru0026@gmail.com>
2018-01-17 22:47:14 -0600

    api request get episode images

---
+++ bookstore-back/src/main/java/com/jonele/bookstore/model/Book.java
@@ -0,0 +1,102 @@
+package com.jonele.bookstore.model;
+
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+/**
+ * Created by Jonelezhang on 1/14/18.
+ */
+@Entity
+public class Book {
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Float unitCost;
+
+    private String isbn;
+
+    private Date publicationDate;
+
+    private Integer nbOfPages;
+
+    private String imageUrl;
+
+    private Language language;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(Float unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getNbOfPages() {
+        return nbOfPages;
+    }
+
+    public void setNbOfPages(Integer nbOfPages) {
+        this.nbOfPages = nbOfPages;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+}
---
+++ bookstore-back/src/main/java/com/jonele/bookstore/model/Language.java
@@ -0,0 +1,8 @@
+package com.jonele.bookstore.model;
+
+/**
+ * Created by Jonelezhang on 1/14/18.
+ */
+public enum Language {
+    ENGLISH, FRENCH, SPANISH, PORTUGUESE, ITALIAN,FINISH, GERMAN, DEUTCH, RUSSIAN
+}
