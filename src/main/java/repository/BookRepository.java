5768a9410e31cabfd5ea437d724147832e1bed12
Jolin Zhang <zhangru0026@gmail.com>
2018-02-03 23:22:00 -0600

    use JPA JPQL transactional repository

---
+++ src/main/java/repository/BookRepository.java
@@ -0,0 +1,49 @@
+package repository;
+
+import com.jonele.bookstore.model.Book;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+/**
+ * Created by Jonelezhang on 2/3/18.
+ */
+@Transactional(SUPPORTS)
+public class BookRepository {
+
+    @PersistenceContext(unitName = "bookStorePU")
+    private EntityManager em;
+
+
+    public Book find(long id){
+        return em.find(Book.class, id);
+
+    }
+    @Transactional(REQUIRED)
+    public Book create(Book book){
+        em.persist(book);
+        return book;
+    }
+
+    @Transactional(REQUIRED)
+    public void delete(long id){
+        em.remove(em.getReference(Book.class, id));
+
+    }
+
+    public List<Book> findAll(){
+        TypedQuery<Book> query = em.createQuery("select b from Book b order by b.title", Book.class);
+        return query.getResultList();
+    }
+    
+    public Long countAll(){
+        TypedQuery<Long>  query =  em.createQuery("select count(b) from Book b", Long.class);
+        return query.getSingleResult();
+    }
+}
