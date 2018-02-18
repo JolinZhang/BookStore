8d1c4b67f5de7396103dfe4433a702eb3a24a921
Jolin Zhang <zhangru0026@gmail.com>
2018-02-17 20:28:52 -0600

    Valid Data

--- src/test/com/jonele/bookstore/repository/BookRepositoryTest.java
+++ src/test/com/jonele/bookstore/repository/BookRepositoryTest.java
@@ -26,6 +26,18 @@ public class BookRepositoryTest {
     @Inject
     private BookRepository bookRepository;
 
+    @Test(expected = Exception.class)
+    public void findWithInvalidId(){
+        bookRepository.find(null);
+    }
+
+    @Test(expected = Exception.class)
+    public void createInvalidBook(){
+        // Create a book
+        Book book = new Book("isbn", null, 12F, 123, Language.ENGLISH, new Date(),"http://banana", "description" );
+        book = bookRepository.create(book);
+    }
+
     @Test
     public void create() throws Exception {
         // Testing counting books
